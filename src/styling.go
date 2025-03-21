c2acea550201f781119cd280085a3ebf307bf255
Abdo-Abuharrus211 <aabuharrus@gmail.com>
2025-03-21 10:35:14 -0700

    Update Styles

--- src/styling.go
+++ src/styling.go
@@ -8,24 +8,6 @@ import (
 )
 
 
-/*
-	TODO: Change color and styles
-	lipgloss.AdaptiveColor{Light: "236", Dark: "248"} //adaptive
-
-
-	CSS style Styles
-
-	var style = lipgloss.NewStyle().
-    Bold(true).
-    Foreground(lipgloss.Color("#FAFAFA")).
-    Background(lipgloss.Color("#7D56F4")).
-    PaddingTop(2).
-    PaddingLeft(4).
-    Width(22)
-	fmt.Println(style.Render("Hello, kitty"))
-
-*/
-
 // Available spinners
 var spinners = []spinner.Spinner{
 	spinner.Line,
